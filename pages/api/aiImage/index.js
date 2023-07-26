71f1475787771ecbc8a33f4d5e016329f3d8b2b2
Harshal Raikwar <harshalraikwar07@gmail.com>
2023-07-27 01:40:15 +0530

    🐛 Fix vercel bug

--- pages/api/aiImage/index.js
+++ pages/api/aiImage/index.js
@@ -13,17 +13,23 @@ const handler = async (req, res) => {
       auth: process.env.AI_API_KEY,
     });
 
-   
+
     const output = await replicate.run(
-      "stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
+      "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf"
+      // "stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
       {
         input: {
           prompt: "Realistic UFO or UAP",
-          image_dimensions: "1024x1024",
-          num_inference_steps: 50,
-          num_outputs: 4,
-          guideance_scale: 6,
-          scheduler: "PNDM" , 
+          // image_dimensions: "1024x1024",
+          // num_inference_steps: 50,
+          // num_outputs: 4,
+          // guideance_scale: 6,
+          // scheduler: "PNDM",
+          image_dimensions: "512x512",
+          num_inference_steps: 12,
+          num_outputs: 1,
+          guideance_scale: 3.5,
+          scheduler: "K_EULER" ,
         },
       },
     );
@@ -31,10 +37,6 @@ const handler = async (req, res) => {
 
     console.log(output)
     res.status(200).json(output)
-    //res.status(200).json([
-     //   'https://replicate.delivery/pbxt/neqGIe66cYuPOUPM0JqokMfqsX9CRYgvkycUxyqlCKUjwJchA/out-0.png'
-    //  ]
-    //);
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal server error', error });
