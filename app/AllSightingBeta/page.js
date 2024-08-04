17e7cc9e062de666d308ff55f7d150759b8f726c
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2024-08-04 10:03:23 +0300

    Update page.js

--- app/AllSightingBeta/page.js
+++ app/AllSightingBeta/page.js
@@ -28,7 +28,7 @@ export default function Page() {
         <div className="bg-secondary w-64 h-1 my-2 rounded-md"></div>
       </div>
     
-        <h2 className="text-xl text-center text-white font-tektur relative"> CTRL + F for search. Click the page and use your mouse/keyboard to scroll to right for more information!</h2>
+        <h2 className="text-xl text-center text-white font-tektur relative"> CTRL+F for search. Type the city or year, etc. Click the page and use your mouse/keyboard to scroll to right for more information!</h2>
 
       <div className="overflow-x-auto mx-4">
         <table className="w-full bg-white shadow-md rounded-lg mt-4">
