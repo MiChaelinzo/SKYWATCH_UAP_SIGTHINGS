bd580e0adeb37b7ff0dc58555363ef33c2062011
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2024-08-04 10:18:17 +0300

    Update page.js

--- app/AllSightingHATCH/page.js
+++ app/AllSightingHATCH/page.js
@@ -25,9 +25,9 @@ export default function Page() {
         </div>
         <div className="bg-secondary w-64 h-1 my-2 rounded-md"></div>
     
-        <h4 className="text-xl text-center text-white font-tektur relative">Larry Hatch was a computer programmer by trade who created one of the most comprehensive databases of UAP/UFO sightings. Learn more about him here: Link https://tinyurl.com/2p95d7ts </h4>
+        <h4 className="text-base text-center text-white font-tektur relative">Larry Hatch was a computer programmer by trade who created one of the most comprehensive databases of UAP/UFO sightings. Learn more about him here: Link https://tinyurl.com/2p95d7ts </h4>
 
-        <h2 className="text-xl text-center text-white font-tektur relative">CTRL+F for search. Type the city or year, etc. Click the page and use your keyboard to scroll to right for more comprehensive information!</h2>
+        <h2 className="text-base text-center text-white font-tektur relative">CTRL+F for search. Type the city or year, etc. Click the page and use your keyboard to scroll to right for more comprehensive information!</h2>
 
       </div>
 
