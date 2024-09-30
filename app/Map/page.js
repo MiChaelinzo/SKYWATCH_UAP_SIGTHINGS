e8f1627d8407c9a30435f98d779095fe0449aba1
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2024-09-30 20:45:51 +0300

    Update page.js

--- app/Map/page.js
+++ app/Map/page.js
@@ -4,7 +4,7 @@ export default function page() {
     return (
         <div className="py-10">
             <div className="grid place-items-center pb-4">
-                <h1 className="text-5xl text-center text-white font-tektur">UAP/UFO Sighting on 3D Globe</h1>
+                <h1 className="text-5xl text-center text-white font-tektur">UAP Sighting on 3D Globe</h1>
                 <h2 className='text-base text-center text-white font-tektur relative'>It uses All Sightings Hatch Data.</h2>
 
                 <div className="bg-secondary w-64 h-1 my-2 rounded-md"></div>
