b8e647d01bf6dfbfb7a861a1cf837bd039fb1548
Michael G. Inso <68110223+MiChaelinzo@users.noreply.github.com>
2025-02-17 13:38:58 +0300

    Update page.js

--- app/Chat/page.js
+++ app/Chat/page.js
@@ -2,6 +2,10 @@
 
 import React from 'react'
 import Chat from '@/components/Chat'
+import AudioRecorder from '@/app/AIBackground/components/AudioRecorder';
+import ScreenShare from '@/app/AIBackground/components/ScreenShare';
+import LiveVideo from '@/app/AIBackground/components/LiveVideo';
+import ChatComponent from '@/app/AIBackground/components/ChatComponent'; // Keep this if you are still using it
 
 export default function Page() {
     return (
